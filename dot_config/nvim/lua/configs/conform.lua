0ba1b81af068d4453fa77cecae7e903c551633db
An Nguyen Ho Duc <intern.nhdan1@tma.com.vn>
2025-07-31 14:36:25 +0700

    update .gitignore_global

--- dot_config/nvim/lua/configs/conform.lua
+++ dot_config/nvim/lua/configs/conform.lua
@@ -1,18 +1,18 @@
 local options = {
-  formatters_by_ft = {
-    lua = { "stylua" },
-    python = { "black" },
-    cpp = { "clang-format" },
-    c = { "clang-format" },
-    javascriptreact = { "prettierd" },
-    javascript = { "prettierd" },
-    json = { "prettierd" },
-  },
+	formatters_by_ft = {
+		lua = { "stylua" },
+		python = { "black" },
+		cpp = { "clang-format" },
+		c = { "clang-format" },
+		javascriptreact = { "prettierd" },
+		javascript = { "prettierd" },
+		json = { "prettierd" },
+	},
 
-  format_on_save = {
-    timeout_ms = 5000,
-    lsp_fallback = true,
-  },
+	format_on_save = {
+		timeout_ms = 5000,
+		lsp_fallback = true,
+	},
 }
 
 return options
