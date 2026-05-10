2956b59826c41009da2589f340fe00e1528a3e4e
DucAnnee <nhdan1110@gmail.com>
2026-05-11 05:33:08 +0700

    add tmux and adjust nvim conf

--- dot_config/nvim/lua/configs/copilot-chat.lua
+++ dot_config/nvim/lua/configs/copilot-chat.lua
@@ -1,16 +1,19 @@
 local icons = require("configs.icons")
 
 return {
-	temperature = 0.1,
+	temperature = 0.2,
 	window = {
 		layout = "vertical",
-		width = 0.3,
+		width = 0.4,
 		border = "rounded",
 		zindex = 100,
 	},
 	model = "gpt-5.4-mini",
 	sticky = {
 		"#buffer:listed",
+		"#file:../AGENTS.md",
+		"#file:./guidelines.md",
+		"#file:./src/components/code-template.txt",
 	},
 	diff = "block",
 	show_help = true,
